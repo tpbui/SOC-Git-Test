08a1f4d720690c4a20b46e3d9f1b5b58e065dbb4
Tra <tpbui@andrew.cmu.edu>
2024-06-04 12:02:55 +0300

    refactor fizzbuzz module

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,5 +3,6 @@ def process(n):
         return "fizz"
     return n
 
-for number in range(1, 10):
-    print(process(number))
+if __name__ == '__main__':
+    for number in range(1, 10):
+        print(process(number))
