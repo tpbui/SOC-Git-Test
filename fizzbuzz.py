76a7f774755ffff9d53b2092b01ae8b77a47b9ea
Tra <tpbui@andrew.cmu.edu>
2024-06-04 11:55:43 +0300

    add fizzbuzz code

---
+++ fizzbuzz.py
@@ -0,0 +1,5 @@
+def process(n):
+    return 1
+
+for number in range(1, 10):
+    print(process(number))
--- test_initial.py
+++ test_initial.py
@@ -2,3 +2,6 @@ from fizzbuzz import process
 
 def test_dummy():
     pass
+
+def test_number_one():
+    assert(process(1) == 1)
